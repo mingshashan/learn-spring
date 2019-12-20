d73aaabc46df0f2c5c60f9ee2be65c6e5c6f0a46
许方杰 <xufj@primeton.com>
2019-12-20 17:15:52 +0800

    update

--- learn-webflux/src/main/java/com/mingshashan/learn/webflux/controller/HelloWebFluxController.java
+++ learn-webflux/src/main/java/com/mingshashan/learn/webflux/controller/HelloWebFluxController.java
@@ -7,12 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 /**
- * @author 犬小哈 (微信号: 小哈学Java)
- * @site 个人网站: www.exception.site
- * @date 2019/4/15
- * @time 下午9:12
- * @discription
- **/
+ *
+ */
 @RestController
 public class HelloWebFluxController {
 
